06b84bd9d839369299ed52c1f134a43b377b94fa
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-10-19 16:18:21 +0500

    Сonsole application createdgit add .!

--- GoPing.go
+++ GoPing.go
@@ -17,4 +17,14 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Println("ping installed in:", pth)
+
+	if len(os.Args) == 2 {
+		out, err := exec.Command("ping", "-c", "3", os.Args[1]).Output()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+		}
+		fmt.Println(string(out))
+	} else {
+		fmt.Fprintf(os.Stderr, "%s\n", "incorrect number of arguments")
+	}
 }
